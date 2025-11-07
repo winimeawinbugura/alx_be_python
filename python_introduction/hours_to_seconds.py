bab608bea5def59ddfa073ab8a6d8419a0ab7ccb
Winime Awinbugur Azumah <winimeawinbugura@gmail.com>
2025-11-07 23:41:50 +0000

    Converting hours to seconds

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,11 @@
+# hours_to_seconds.py
+# This script converts hours into seconds
+
+# Given value
+hours = 2
+
+# Conversion formula
+seconds = hours * 3600
+
+# Display the result
+print(f"{hours} hour(s) is {seconds} seconds.")
